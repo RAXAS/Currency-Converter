6f5220941c9cf48209f616cd5f0e3a6bab4b0b4d
Dmitry Byk <dima-pinsk@mail.ru>
2022-03-30 14:32:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,19 +5,21 @@ import json
 TOKEN = "5216680764:AAFpQ4yUAtdqPMkq58PUmJiYP5YnE5ld0f0"
 bot = telebot.TeleBot(TOKEN)
 
-value1, value2 = input("Введите USD или EUR\n"), input("Введите USD или EUR\n")
 
-response = requests.get(f"https://min-api.cryptocompare.com/data/price?fsym={value1}&tsyms={value2}")
+elements = list(input("Введите числа через пробел:\n").split())
+
+print(elements[0])
+response = requests.get(f"https://min-api.cryptocompare.com/data/price?fsym={elements[0]}&tsyms={elements[1]}")
 response.text
 print(response.text)
 
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    sent = bot.send_message(message.chat.id, 'Как тебя зовут?')
-    bot.register_next_step_handler(sent, hello)
+    sent = bot.send_message(message.chat.id, 'Введите валюты, которые хотите конвертировать в формате: первая валюта, вторая валюта, колличество первой валюты')
+    bot.register_next_step_handler(sent, сonverter)
 
-def hello(message):
+def сonverter(message):
     bot.send_message(message.chat.id, 'Привет, {name}. Рад тебя видеть.'.format(name=message.text))
 
 bot.polling()
