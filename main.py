421cf7900c5b96b59aacfdd63dab1ad8740b5e69
Dmitry Byk <dima-pinsk@mail.ru>
2022-04-03 01:12:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,19 +5,23 @@ import json
 TOKEN = "5216680764:AAFpQ4yUAtdqPMkq58PUmJiYP5YnE5ld0f0"
 bot = telebot.TeleBot(TOKEN)
 
-@bot.message_handler(commands=['start'])
+@bot.message_handler(commands=['start', 'help'])
 def start(message):
-    sent = bot.send_message(message.chat.id, 'Введите валюты, которые хотите конвертировать в формате: первая валюта, вторая валюта, колличество первой валюты. Например: usd rub 100')
-    bot.register_next_step_handler(sent, сonverter)
+    sent = bot.send_message(message.chat.id, 'Введите буквенные коды валют, которые хотите конвертировать в формате: <имя валюты, цену которой вы хотите узнать>, <имя валюты в которой надо узнать цену первой валюты>, <колличество первой валюты>. Например: usd rub 100')
+    bot.register_next_step_handler(sent)
 
+@bot.message_handler(commands=['values'])
+def values(message):
+    available_values = bot.send_message(message.chat.id, 'Доступны все буквенные коды валют криптовалют, DeFi и NFT')
+    bot.register_next_step_handler(available_values, сonverter)
+@bot.message_handler()
 def сonverter(message):
     elements = message.text
     elements = list(elements.split(" "))
-    #elements = elements.text
     response = requests.get(f"https://min-api.cryptocompare.com/data/price?fsym={elements[0]}&tsyms={elements[1]}")
     a = json.loads(response.text)
     b = list(a.values())
     c = float(elements[2]) * float(b[0])
-    bot.send_message(message.chat.id, f'Ответ {c}')
+    bot.send_message(message.chat.id, f'{elements[2]} {elements[0]} = {c} {elements[1]}')
 
 bot.polling()
