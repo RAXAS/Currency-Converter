edc2aef3dee2da48cd97a3fc7f5a93d189d6addb
Dmitry Byk <dima-pinsk@mail.ru>
2022-04-03 17:48:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,30 @@
 import telebot
-import requests
-import json
+from script import TOKEN
+from extensions import ConversionException, ValueConverter
 
-TOKEN = "5216680764:AAFpQ4yUAtdqPMkq58PUmJiYP5YnE5ld0f0"
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start', 'help'])
 def start(message):
-    sent = bot.send_message(message.chat.id, 'Введите буквенные коды валют, которые хотите конвертировать в формате: <имя валюты, цену которой вы хотите узнать>, 
-<имя валюты в которой надо узнать цену первой валюты>, <колличество первой валюты>. Например: usd rub 100')
+    sent = bot.send_message(message.chat.id, 'Введите буквенные коды валют, которые хотите конвертировать в формате: <имя валюты, цену которой вы хотите узнать>, <имя валюты в которой надо узнать цену первой валюты>, <колличество первой валюты>. Например: usd rub 100')
     bot.register_next_step_handler(sent)
 
 @bot.message_handler(commands=['values'])
 def values(message):
-    available_values = bot.send_message(message.chat.id, 'Доступны все буквенные коды валют криптовалют, DeFi и NFT')
+    available_values = bot.send_message(message.chat.id, 'Доступны все буквенные коды валют, криптовалют, DeFi и NFT')
     bot.register_next_step_handler(available_values, сonverter)
 
 @bot.message_handler()
 def сonverter(message):
-    elements = message.text
-    elements = list(elements.split(" "))
-    response = requests.get(f"https://min-api.cryptocompare.com/data/price?fsym={elements[0]}&tsyms={elements[1]}")
-    a = json.loads(response.text)
-    b = list(a.values())
-    c = float(elements[2]) * float(b[0])
-    bot.send_message(message.chat.id, f'{elements[2]} {elements[0]} = {c} {elements[1]}')
+
+    try:
+        quote, base, amount = message.text.split(" ")
+    except ValueError:
+        raise ConversionException("Введено неверное колличество параметров")
+
+    total_base = ValueConverter.convert(quote, base, amount)
+    #response = json.loads(requests.get(f"https://min-api.cryptocompare.com/data/price?fsym={quote}&tsyms={base}").text)
+
+    bot.send_message(message.chat.id, f'{amount} {quote} = {c} {base}   {total_base}')
 
 bot.polling()
