950879b18e4dfd9da1fe05a272ca09a5f2907e7e
Dmitry Byk <dima-pinsk@mail.ru>
2022-04-07 09:52:38 +0300

    Update extensions.py

--- extensions.py
+++ extensions.py
@@ -11,6 +11,11 @@ class ValueConverter:
     @staticmethod
     def get_price(quote: str, base: str, amount: str):
 
+        try:
+            amount = float(amount)
+        except ValueError:
+            raise ConversionException(f"Не удалось обработать колличество {amount}")
+        
         if int(amount) < 0:
             raise ConversionException("Колличество валюты не может быть ниже нуля")
 
@@ -27,11 +32,7 @@ class ValueConverter:
         except KeyError:
             raise ConversionException(f'Не смог обработать валюту {base}')
 
-        try:
-            amount = float(amount)
-        except ValueError:
-            raise ConversionException(f"Не удалось обработать колличество {amount}")
-
+        
         response = requests.get(f"https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}")
         total_base = float(json.loads(response.content)[base_ticker]) * amount
         return total_base
